8017776d6a43b252b074915628406b2e5c51702c
crazyfuy <marcospineiro@hotmail.com>
2022-08-22 16:58:46 +0200

    Agregar archivos de proyecto.

---
+++ bootstrapperv2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace bootstrapperv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2ProgressBar1 = new Guna.UI2.WinForms.Guna2ProgressBar();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // guna2ProgressBar1
+            // 
+            this.guna2ProgressBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.guna2ProgressBar1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.guna2ProgressBar1.Location = new System.Drawing.Point(171, 154);
+            this.guna2ProgressBar1.Name = "guna2ProgressBar1";
+            this.guna2ProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.guna2ProgressBar1.ProgressColor2 = System.Drawing.Color.DarkMagenta;
+            this.guna2ProgressBar1.Size = new System.Drawing.Size(12, 30);
+            this.guna2ProgressBar1.TabIndex = 0;
+            this.guna2ProgressBar1.Text = "guna2ProgressBar1";
+            this.guna2ProgressBar1.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.BorderRadius = 15;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.DragForm = false;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 47);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "GHOST";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkGray;
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(333, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "checking version...";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(357, 152);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2ProgressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ProgressBar guna2ProgressBar1;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ bootstrapperv2/Form1.cs
@@ -0,0 +1,190 @@
+﻿using FluentTransitions;
+using FluentTransitions.Methods;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bootstrapperv2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Location = new Point(12, -46);
+            label2.Location = new Point(12, -46);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Transition.RunChain
+            (
+            Transition
+            .With(label2, nameof(Top), 54)
+            .With(label1, nameof(Top), 9)
+            .Build(new EaseInEaseOut(TimeSpan.FromSeconds(1)))
+            );
+
+            Transition.RunChain
+            (
+            Transition
+            .With(this, nameof(Opacity), 1.0)
+            .Build(new EaseInEaseOut(TimeSpan.FromSeconds(1))),
+            Transition
+            .With(guna2ProgressBar1, nameof(Top), 122)
+            .With(guna2ProgressBar1, nameof(Left), 0)
+            .With(guna2ProgressBar1, nameof(Width), 357)
+            .HookOnCompletionInUiThread(SynchronizationContext.Current, () => finishanimating())
+            .Build(new EaseInEaseOut(TimeSpan.FromSeconds(1)))
+            );
+
+        }
+
+        private async void finishanimating()
+        {
+            try
+            {
+                label2.Text = "checking version...";
+                WebClient wb = new WebClient();
+                string version = wb.DownloadString("https://ghost-storage.7m.pl/ver.txt");
+
+                if (int.Parse(version) > 16) //If the server replies with a int higher than 16 it will stop the launching
+                {
+                    DialogResult dialogResult = MessageBox.Show("Looks like your bootstrapper is outdated, do you want to join the discord?", "Outdated bootstrapper", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        Process.Start("https://discord.gg/FnnHGWpgk8");
+                    }
+                    return;
+                }
+            }
+            catch //Catches any error that may occur checking for updates
+            {
+                DialogResult dialogResult = MessageBox.Show("Couldn't check for updates! Do you want to join the discord?", "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Process.Start("https://discord.gg/FnnHGWpgk8");
+                }
+                return;
+            }
+            guna2ProgressBar1.Value = 30;
+            label2.Text = "Creating directories";
+            if (!Directory.Exists("bin"))
+            {
+
+                Directory.CreateDirectory("bin");
+            }
+
+            if (!Directory.Exists("bin/Scripts"))
+            {
+                Directory.CreateDirectory("bin/Scripts");
+            }
+
+            if (!Directory.Exists("bin/lite"))
+            {
+                Directory.CreateDirectory("bin/lite");
+
+            }
+
+            label2.Text = "Downloading some files...";
+            guna2ProgressBar1.Value = 50;
+            await Task.Delay(2000);
+            if (!File.Exists("bin/Ghost.exe.config"))
+            {
+                using (WebClient config = new WebClient())
+                {
+                    config.DownloadFile(new Uri("https://ghost-storage.7m.pl/Ghost.exe.config"), Application.StartupPath + "/bin/Ghost.exe.config");
+                }
+
+            }
+
+
+            if (!File.Exists("bin/lite/FastColoredTextBox.dll"))
+            {
+                using (WebClient fastcolored = new WebClient())
+                {
+                    fastcolored.DownloadFile(new Uri("https://ghost-storage.7m.pl/FastColoredTextBox"), Application.StartupPath + "/bin/lite/FastColoredTextBox.dll");
+                }
+            }
+
+            if (!File.Exists("bin/lite/FluxAPI.dll"))
+            {
+                using (WebClient fluxapi = new WebClient())
+                {
+                    fluxapi.DownloadFile(new Uri("https://ghost-storage.7m.pl/FluxAPI.dll"), Application.StartupPath + "/bin/lite/FluxAPI.dll");
+                }
+            }
+            if (!File.Exists("bin/lite/ghost_lite.exe"))
+            {
+                using (WebClient ghlite = new WebClient())
+                {
+                    ghlite.DownloadFile(new Uri("https://ghost-storage.7m.pl/Ghost_lite"), Application.StartupPath + "/bin/lite/Ghost_lite.exe");
+                }
+            }
+            label2.Text = "Finishing...";
+            guna2ProgressBar1.Value = 80;
+            launchghost();
+            
+        }
+
+        private void launchghost()
+        {
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
+                    wc.DownloadFile(new Uri("https://ghost-storage.7m.pl/Ghost2.1"), Application.StartupPath + "/bin/ghost.exe");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error occurred");
+            }
+
+            Completed();
+        }
+
+        private void Completed()
+        {
+            if (File.Exists("bin/ghost.exe"))
+            {
+                var startInfo = new ProcessStartInfo();
+                startInfo.WorkingDirectory = "bin";
+                startInfo.FileName = "ghost.exe";
+                Process proc = Process.Start(startInfo);
+                guna2ProgressBar1.Value = 100;
+                Transition
+                .With(this, nameof(Opacity), 0.0)
+                .With(label1, nameof(Top), -5)
+                .With(label2, nameof(Top), -5)
+                .With(guna2ProgressBar1, nameof(Width), 0)
+                .With(guna2ProgressBar1, nameof(Left), 171)
+                .With(guna2ProgressBar1, nameof(Top), 154)
+                .HookOnCompletionInUiThread(SynchronizationContext.Current, () => Application.Exit())
+                .EaseInEaseOut(TimeSpan.FromSeconds(0.5));
+            }
+            else
+            {
+                MessageBox.Show("The GHOST executable has been deleted, this is usually caused by an active antivirus","Executable missing");
+            }
+        }
+
+        private void Wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            //guna2ProgressBar1.Value = e.ProgressPercentage;
+        }
+    }
+}
